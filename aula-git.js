488db889285a7b147aded2f45d4cc3d5dbb2ffc4
Douglas Santana Costa <douglas.santana1777@gmail.com>
2022-08-29 20:24:25 -0300

    fiz um git pull, add, commit e por fim o Push

--- aula-git.js
+++ aula-git.js
@@ -19,7 +19,7 @@ if (idade >= 18) {
 }
 console.log("===========================================================================")
 
-var idade = 32
+var idade = 31
 var sexo = 'M'
 var nome = "Billy"
 if (idade >= 18) {
